61e8e5f182fd00cec93edad15b013eac32a272f7
Matt Kmiecik <mkmiecik14@gmail.com>
2021-06-04 17:03:37 -0500

    progress on ppt exploration

--- ppt-explore.R
+++ ppt-explore.R
@@ -177,4 +177,212 @@ ggplot(cpm_ss, aes(force, color = site, fill = site)) +
   theme(legend.position = "bottom")
 
 # summary stats
+cpm_sum <- 
+  cpm_ss %>%
+  group_by(trial, site) %>%
+  summarise(
+    M = mean(force, na.rm = TRUE), 
+    N = sum(!is.na(force)), 
+    SD = sd(force, na.rm = TRUE),
+    SEM = SD/sqrt(N),
+    LL = as.numeric(t.test(force, conf.level = 0.95)$conf.int[1]),
+    UL = as.numeric(t.test(force, conf.level = 0.95)$conf.int[2])
+    ) %>%
+  ungroup()
 
+# Summary data of all trials
+pd <- position_dodge(width = .2) 
+ggplot(cpm_sum, aes(trial, M, group = site, color = site)) +
+  geom_point(data = cpm_ss, aes(y = force), position = pj, shape = 1, alpha = 1/3) +
+  geom_point(position = pd) +
+  geom_errorbar(aes(ymin = LL, ymax = UL), width = .2, position = pd) +
+  geom_path(position = pd) +
+  theme_minimal() +
+  labs(x = "Trial", y = "Mean Force (Newtons)", caption = "95% CI error bars.") +
+  scale_color_brewer(palette = "Set1") +
+  theme(legend.position = "bottom")
+
+# Let's just focus on trial 2 - trial 1 difference
+# eliminate anyone without repeated measures here
+cpm_ss_wide <-
+  cpm_ss %>% 
+  filter(trial == 1 | trial == 2) %>%
+  select(ss, test, trial, site, force) %>%
+  pivot_wider(id_cols = ss, names_from = c(site, trial), values_from = force) %>%
+  mutate(lKnee = lKnee_2 - lKnee_1, lShoulder = lShoulder_2 - lShoulder_1)
+
+# converts back to long to simplify drops
+cpm_ss_long <- 
+  cpm_ss_wide %>%
+  select(ss, lKnee, lShoulder) %>%
+  pivot_longer(
+    cols = c(lKnee, lShoulder), 
+    names_to = "site", 
+    values_to = "diff_force"
+    ) %>%
+  filter(complete.cases(diff_force)) # elimiates missing data
+  
+# seperate dfs for knee and shoulder
+cpm_lKnee <- cpm_ss_long %>% filter(site %in% "lKnee")          # lKnee
+cpm_lShoulder <- cpm_ss_long %>% filter(site %in% "lShoulder")  # lShoulder
+
+# LEFT KNEE SUBJECT-WISE
+cpm_ss_lKnee <-
+  cpm_ss %>% 
+  filter(
+    site %in% "lKnee",
+    ss %in% cpm_lKnee$ss, # only ss with complete knee data
+    trial == 1 | trial == 2 # trial 1 vs. 2
+    )
+
+# LEFT KNEE SUMMARY
+cpm_ss_lKnee_sum <- 
+  cpm_ss_lKnee %>%
+  group_by(trial) %>%
+  summarise(
+    M = mean(force), 
+    N = n(), 
+    SD = sd(force), 
+    SEM = SD/sqrt(N),
+    LL = as.numeric(t.test(force, conf.level = 0.95)$conf.int[1]),
+    UL = as.numeric(t.test(force, conf.level = 0.95)$conf.int[2])
+    ) %>%
+  ungroup()
+
+# Raw data - left knee
+pd <- position_dodge(width = .1) 
+ggplot(cpm_ss_lKnee_sum, aes(trial, M, group = 1)) +
+  geom_point(data = cpm_ss_lKnee, aes(y = force, group = ss), shape = 16, position = pd, color = ghibli_palettes$MononokeMedium[3]) +
+  geom_path(data = cpm_ss_lKnee, aes(y = force, group = ss), position = pd, color = ghibli_palettes$MononokeMedium[3], alpha = 1/3) +
+  labs(x = "Trial", y = "Force") +
+  scale_x_continuous(breaks = c(1, 2)) +
+  theme_classic()
+
+# Mean CPM effect - left knee
+pd <- position_dodge(width = .1) 
+ggplot(cpm_ss_lKnee_sum, aes(trial, M)) +
+  geom_bar(stat = "identity", color = "black", fill = ghibli_palettes$MononokeLight[3], width = .3) +
+  geom_errorbar(aes(ymin = LL, ymax = UL), width = .1, color = "black") +
+  labs(x = "Trial", y = "Force", caption = "95% CI error bars.") +
+  scale_x_continuous(breaks = c(1, 2)) +
+  theme_classic()
+
+
+# LEFT SHOULDER SUBJECT-WISE
+cpm_ss_lShoulder <-
+  cpm_ss %>% 
+  filter(
+    site %in% "lShoulder",
+    ss %in% cpm_lShoulder$ss, # only ss with complete knee data
+    trial == 1 | trial == 2 # trial 1 vs. 2
+  )
+
+# LEFT KNEE SUMMARY
+cpm_ss_lShoulder_sum <- 
+  cpm_ss_lShoulder %>%
+  group_by(trial) %>%
+  summarise(
+    M = mean(force), 
+    N = n(), 
+    SD = sd(force), 
+    SEM = SD/sqrt(N),
+    LL = as.numeric(t.test(force, conf.level = 0.95)$conf.int[1]),
+    UL = as.numeric(t.test(force, conf.level = 0.95)$conf.int[2])
+  ) %>%
+  ungroup()
+
+# Raw data - left shoulder
+pd <- position_dodge(width = .1) 
+ggplot(cpm_ss_lShoulder_sum, aes(trial, M, group = 1)) +
+  geom_point(data = cpm_ss_lShoulder, aes(y = force, group = ss), shape = 16, position = pd, color = ghibli_palettes$MononokeMedium[3]) +
+  geom_path(data = cpm_ss_lShoulder, aes(y = force, group = ss), position = pd, color = ghibli_palettes$MononokeMedium[3], alpha = 1/3) +
+  labs(x = "Trial", y = "Force") +
+  scale_x_continuous(breaks = c(1, 2)) +
+  theme_classic()
+
+# Mean CPM effect - left knee
+pd <- position_dodge(width = .1) 
+ggplot(cpm_ss_lShoulder_sum, aes(trial, M)) +
+  geom_bar(stat = "identity", color = "black", fill = ghibli_palettes$MononokeLight[3], width = .3) +
+  geom_errorbar(aes(ymin = LL, ymax = UL), width = .1, color = "black") +
+  labs(x = "Trial", y = "Force", caption = "95% CI error bars.") +
+  scale_x_continuous(breaks = c(1, 2)) +
+  theme_classic()
+
+# Temporal Summation ----
+ts_ss <- 
+  ppt_data_grps %>%
+  filter(test %in% "TS", visit == 1) %>%
+  filter(complete.cases(force)) # elimiates trials with missing/max out
+
+# plotting TS trajectories
+pd <- position_dodge(width = .2) 
+ggplot(ts_ss, aes(as.factor(trial), force, group = ss)) +
+  geom_point(position = pd, shape = 16, alpha = 1/3) +
+  geom_path(position = pd, alpha = 1/3) +
+  labs(x = "Trial", y = "Force (Newtons)") +
+  theme_classic()
+
+# TS summary by trial
+ts_sum <- 
+  ts_ss %>%
+  group_by(trial) %>%
+  summarise(
+    M = mean(force), 
+    N = n(), 
+    SD = sd(force), 
+    SEM = SD/sqrt(N),
+    LL = as.numeric(t.test(force, conf.level = 0.95)$conf.int[1]),
+    UL = as.numeric(t.test(force, conf.level = 0.95)$conf.int[2])
+    ) %>%
+  ungroup()
+
+# summary plot
+ggplot(ts_sum, aes(trial, M)) +
+  geom_point(
+    data = ts_ss, 
+    aes(y = force), 
+    shape = 1, 
+    position = pj, 
+    color = ghibli_palettes$YesterdayMedium[2], 
+    alpha = 1/3
+    ) +
+  geom_bar(
+    stat = "identity", 
+    color = "black", 
+    fill = ghibli_palettes$YesterdayMedium[2], 
+    alpha = 1/2
+    ) +
+  geom_errorbar(aes(ymin = LL, ymax = UL), width = .2) +
+  labs(x = "Trial", y = "Mean Force (Newtons)", caption = "95% CI error bars.") +
+  theme_classic()
+
+ggplot(ts_ss, aes(force)) +
+  geom_histogram(binwidth = 3) +
+  facet_wrap(~trial)
+
+# Linear modeling to look at the intercept and slope
+# models
+ts_mod <- 
+  ts_ss %>%
+  nest_by(ss) %>%
+  mutate(mod = list(lm(force ~ 1 + scale(trial, scale = FALSE), data = data)))
+
+# estimates
+ts_est <- 
+  ts_mod %>%
+  summarise(broom::tidy(mod)) %>%
+  ungroup() %>%
+  mutate(
+    term = gsub("[\\(\\)]", "", term), 
+    term = gsub("scaletrial, scale = FALSE", "trial_mc", term)
+    )
+ggplot(ts_est %>% filter(term %in% "Intercept"), aes(estimate)) +
+  geom_histogram()
+
+ggplot(ts_est %>% filter(term %in% "trial_mc"), aes(estimate)) +
+  geom_histogram()
+
+
+ggplot(ts_est %>% filter(term %in% "trial_mc"), aes(estimate, reorder(ss, estimate))) +
+  geom_point()
