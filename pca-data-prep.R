671747c8a69806ca601e3a8bd51af76810179aaf
Matt Kmiecik <mkmiecik14@gmail.com>
2021-06-14 16:43:15 -0500

    progress on pca data prep

--- pca-data-prep.R
+++ pca-data-prep.R
@@ -120,8 +120,6 @@ aud_pca_data <-
 #     #
 #######
 
-#! Multiply these by -1 as higher value = less sensitive
-
 # External Thresholds
 ext_N_pca_data <- 
   left_join(ss_codes_ss, ppt_data, by = "ss") %>%
@@ -229,6 +227,45 @@ ts_pca_data <-
 #                   #
 #####################
 
+# First step is to align record numbers with ss id's across arms 1 and 2
+arm1_temp <- 
+  bladder_data %>% 
+  filter(redcap_event_name %in% "assessment_visit_1_arm_1") %>%
+  left_join(., ss_codes, by = c("record_number" = "arm1r")) %>%
+  mutate(ss = ss.y) %>%
+  select(-ss.x, -ss.y, -arm1ref, -arm2r)
+arm2_temp <- 
+  bladder_data %>% 
+  filter(redcap_event_name %in% "assessment_visit_1_arm_2") %>%
+  left_join(., ss_codes, by = c("record_number" = "arm2r")) %>%
+  mutate(ss = ss.y) %>%
+  select(-ss.x, -ss.y, -arm1ref, -arm1r)
+
+# this should now have correct record numbers to ss ids
+bladder_data_ss <- 
+  # combines arm1 and arm2 while removing ss that are KIDS or EXCLUDE (see above)
+  left_join(ss_codes_ss, bind_rows(arm1_temp, arm2_temp), by = "ss")
+
+# Extracting specific bladder task data measures
+bladder_pca_data <- 
+  bladder_data_ss %>%
+  select(
+    ss, 
+    bs_pain = bt3_baselinepain, 
+    bs_vol = bt4_baselinevol,
+    #fs_time = bt10a_fstime,
+    fs_urg = bt10a_fsurg,
+    fs_pain = bt10a_fspain,
+    fs_vol = bt10a_fsvol,
+    #fu_time = bt10b_futime,
+    fu_urg = bt10b_fuurg,
+    fu_pain = bt10b_fupain,
+    fu_vol = bt10b_fuvol,
+    #mt_time = bt10c_mttime,
+    mt_urg = bt10c_mturg,
+    mt_pain = bt10c_mtpain,
+    mt_vol = bt12a_mtvol
+  )
 
 
 ###################
@@ -238,7 +275,65 @@ ts_pca_data <-
 ###################
 
 # External and Internal
+after_pain_long <- 
+  redcap_ppt_data_ss %>%
+  select(
+    ss,
+    bl_rshoulder = pt2a_shoulder,
+    bl_rhip = pt2b_hip,
+    bl_rknee = pt2c_knee,
+    bl_forehead = pt2d_forehead,
+    bl_vaginal = pt2e_vag,
+    t1_rshoulder = pt3a1,
+    t1_rhip = pt3b1,
+    t1_rknee = pt3c1,
+    t1_forehead = pt3d1,
+    t2_rshoulder = pt3a2,
+    t2_rhip = pt3b2,
+    t2_rknee = pt3c2,
+    t2_forehead = pt3d2,
+    t1_12 = pt3e1,
+    t1_5 = pt3f1,
+    t1_6 = pt3g1,
+    t1_7 = pt3h1,
+    t2_12 = pt3e2,
+    t2_5 = pt3f2,
+    t2_6 = pt3g2,
+    t2_7 = pt3h2
+  ) %>%
+  # subtracting out baseline from each trial (external PPTs)
+  mutate(
+    t1_rshoulder = t1_rshoulder - bl_rshoulder,
+    t1_rhip =  t1_rhip - bl_rhip,
+    t1_rknee =  t1_rknee - bl_rknee,
+    t1_forehead = t1_forehead - bl_forehead,
+    t2_rshoulder =  t2_rshoulder - bl_rshoulder,
+    t2_rhip = t2_rhip - bl_rhip,
+    t2_rknee = t2_rknee - bl_rknee,
+    t2_forehead = t2_forehead - bl_forehead
+  ) %>%
+  # subtracting out baseline from each trial (internal PPTs)
+  mutate(across(t1_12:t2_7, ~ .x - bl_vaginal)) %>%
+  select(-bl_vaginal, -bl_rshoulder, -bl_rhip, -bl_rknee, -bl_forehead) %>% # removes baseline
+  pivot_longer(!ss) %>%
+  separate(name, into = c("time", "site"))
 
+# Averages pain across trials
+after_pain_sum <- 
+  after_pain_long %>% 
+  group_by(ss, site) %>% 
+  summarise(after_pain = mean(value, na.rm = TRUE)) %>%
+  ungroup()
+
+# Widens data
+afterpain_pca_data <-
+  after_pain_sum %>% 
+  pivot_wider(
+    id_cols = ss, 
+    names_from = site,
+    names_prefix = "after_pain_",
+    values_from = after_pain
+    )
 
 ######################
 #                    #
@@ -246,17 +341,46 @@ ts_pca_data <-
 #                    #
 ######################
 
+# Bladder task
+bladder_mcgill_ss <- 
+  bladder_data_ss %>%
+  select(
+    ss,
+    bladder_sharp = bt11a_sharp, 
+    bladder_pressing = bt11b_pressing,
+    bladder_dull = bt11d_dull,
+    bladder_prickling = bt11d_prickling
+    )
+
+# Internal PPTs
+vaginal_mcgill_ss <- 
+  redcap_ppt_data_ss %>%
+  select(
+    ss,
+    vag_sharp = pt3_5a_sharp,
+    vag_pressing = pt3_5b_pressing,
+    vag_dull = pt3_5c_dull,
+    vag_prickling = pt3_5d_prickling
+  ) 
+
+mcgill_pca_data <- inner_join(bladder_mcgill_ss, vaginal_mcgill_ss, by = "ss")
+
 #############
 #           #
 # COLD PAIN #
 #           #
 #############
 
+redcap_ppt_data_ss %>%
+  select(ss, watertemp = pt_watertemp, coldpain = pt4b_cpmwater)
+
+
+coldpain_data %>% filter(complete.cases(.))
+
 # be sure to correct for water temp
 
 
   
-
 
 
 
