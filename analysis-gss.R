cfe2c297aa8f1d92656c5abc3d840880e9fa72e0
Matt Kmiecik <mkmiecik14@gmail.com>
2022-06-21 17:01:43 -0500

    gss analysis

--- analysis-gss.R
+++ analysis-gss.R
@@ -2,13 +2,13 @@
 # Matt Kmiecik
 # Started 20 June 2022
 
-#* Purpose: creat and analyze the GSS variable and compare it to MMH
+#* Purpose: create and analyze the GSS variable and compare it to MMH
 
 # Prep workspace
 source("r-prep.R")
 
 # Loads data
-load("../output/gss-data.rda")
+load("../output/gss-data-wide.rda.")
 load("../output/mmh-res.RData") # pca res
 load("../output/ss-codes.RData")
 
@@ -19,36 +19,300 @@ fi <-
   left_join(., ss_codes %>% select(ss, group), by = "ss") %>%
   select(ss, group, V1:V40)
 
+# creates long df
+gss_data_long <- gss_data_wide %>% pivot_longer(-ss)
+
 # Histogram
-ggplot(gss_data, aes(sum)) +
+ggplot(
+  gss_data_long %>% filter(name %in% c("pain_sites", "sa", "sens")), 
+  aes(value)
+  ) +
   geom_histogram(binwidth = 1) +
   labs(x = "Sum", y = "Frequency") +
   theme_bw() +
-  facet_wrap(~meas)
+  facet_wrap(~name)
 
-ggplot(gss_data, aes(meas, sum, fill = meas)) +
+ggplot(
+  gss_data_long %>% filter(name %in% c("pain_sites_z", "sa_z", "sens_z", "gss")), 
+  aes(value)
+) +
+  geom_histogram(binwidth = 1) +
+  labs(x = "Sum", y = "Frequency") +
+  theme_bw() +
+  facet_wrap(~name)
+
+# BOXPLOTs
+ggplot(
+  gss_data_long %>% filter(name %in% c("pain_sites", "sa", "sens")), 
+  aes(name, value, fill = name)) +
   geom_boxplot() +
   labs(x = "GSS Component", y = "Sum") +
   theme_bw() +
   scale_fill_manual(values = c(ghibli_palettes$KikiLight[3:5])) +
   theme(legend.position = "none")
 
-# Computing Z scores for combination - - - -
+# BOXPLOTs
+ggplot(
+  gss_data_long %>% filter(name %in% c("pain_sites_z", "sa_z", "sens_z", "gss")), 
+  aes(name, value, fill = name)) +
+  geom_boxplot() +
+  labs(x = "GSS Component", y = "Z-Score") +
+  theme_bw() +
+  scale_fill_manual(values = c(ghibli_palettes$MononokeLight[3:6])) +
+  theme(legend.position = "none")
 
-# wide format
-gss_data_wide <-
-  gss_data %>% 
+# Brings in pelvic pain data - - - -
+load("../output/complete-extra-annual-data.Rdata") # annual data
+
+# Calculating pelvic pain outcome variable as an average of:
+# 1) urination_pain_last_week
+# 2) bowel_mov_pain_last_week
+# 3) mens_nonmens_pain_week
+
+# narrows data
+pelvic_pain_data <- 
+  complete_extra_annual_data %>% 
+  select(
+    ss, 
+    year, 
+    timestamp, 
+    days_from_baseline, 
+    urination_pain_last_week, 
+    bowel_mov_pain_last_week, 
+    mens_nonmens_pain_week
+  )
+
+# long-format
+pelvic_pain_data_long <- 
+  pelvic_pain_data %>%
+  select(-timestamp) %>%
+  pivot_longer(cols = c(-ss, -year, -days_from_baseline))
+
+# narrow down the participants in PCA results
+# PCA subject numbers
+pca_ss <- as.numeric(rownames(pca_res$Fixed.Data$ExPosition.Data$fi)) 
+
+# retains only those in PCA
+pelvic_pain_data_pca_ss <- pelvic_pain_data %>% filter(ss %in% pca_ss)
+length(unique(pelvic_pain_data_pca_ss$ss)) == length(pca_ss) # same ss
+
+############################
+#                          #
+# AVERAGED PELVIC PAIN VAS #
+#                          #
+############################
+
+pelvic_pain_data_pca_ss %>% filter(!complete.cases(.))
+
+# computes average here
+pelvic_pain_avg <- 
+  pelvic_pain_data_pca_ss %>%
+  filter(complete.cases(.)) %>% # removes those subjects with missing data
+  pivot_longer(c(-ss, -year, -timestamp, -days_from_baseline)) %>%
+  group_by(ss, year, timestamp, days_from_baseline) %>%
+  summarise(pelvic_pain = mean(value), n = n()) %>%
+  ungroup()
+
+pelvic_pain_avg %>% filter(n != 3) # all have three observations/timepoint
+pelvic_pain_avg %>% filter(is.na(pelvic_pain)) # no missing here
+
+mod_data <- 
+  pelvic_pain_avg %>%
   pivot_wider(
     id_cols = ss, 
-    names_from = meas, 
-    values_from = sum,
-    values_fn = length
+    names_from = year, 
+    names_prefix = "year", 
+    values_from = pelvic_pain
+    ) %>%
+  left_join(., gss_data_wide, by = "ss") %>%
+  left_join(., fi %>% select(ss, V1:V3), by = "ss") %>%
+  mutate(
+    across(
+      .cols = c(year0, gss, V1), 
+      .fns = ~as.numeric(scale(.x, scale = FALSE)),
+      .names = "{.col}_mc"
+      )
     )
 
-test <- gss_data_wide %>% filter(is.na(pain_site))
+# Models
+year0_mod <- lm(year0 ~ 1 + gss_mc + V1_mc, data = mod_data)
+summary(year0_mod)
+check_model(year0_mod)
 
-# gss data with the pca subjects only and with complete data 
-gss_data_pca <- 
-  gss_data_wide %>% 
-  filter(ss %in% fi$ss) %>% # pca subjects only
-  filter(is.na(pain_site)) # complete data only
+year1_mod <- lm(year1 ~ 1 + year0_mc + gss_mc + V1_mc, data = mod_data)
+summary(year1_mod)
+check_model(year1_mod)
+
+year2_mod <- lm(year2 ~ 1 + year0_mc + gss_mc + V1_mc, data = mod_data)
+summary(year2_mod)
+check_model(year2_mod)
+
+year3_mod <- lm(year3 ~ 1 + year0_mc + gss_mc + V1_mc, data = mod_data)
+summary(year3_mod)
+check_model(year3_mod)
+
+year4_mod <- lm(year4 ~ 1 + year0_mc + gss_mc + V1_mc, data = mod_data)
+summary(year4_mod)
+check_model(year4_mod)
+
+# Zero order correlations
+set.seed(14) # sets seed for reproducible boostrapping
+
+# Computes bootstrapped correlations
+zero_order_cors <-
+  psych::corr.test(
+    mod_data %>% select(year0_mc, gss_mc, V1_mc),
+    use = "pairwise",
+    method = "pearson", 
+    adjust = "none",
+    ci = TRUE,
+    minlength = 100 # extends the abrreviations
+  )
+zero_order_cors$ci # the results
+
+# correlation results as tibble for plotting
+cor_res <- 
+  as_tibble(zero_order_cors$ci, rownames = "var") %>%
+  separate(var, into = c("var1", "var2"), sep = "-") %>%
+  mutate(var2 = fct_relevel(var2, c("gss_mc", "V1_mc")))
+
+cor_plot <-
+  ggplot(cor_res %>% filter(var1 == "year0_mc"), aes(var2, r)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = lower, ymax = upper), width = .2) +
+  coord_cartesian(ylim = c(-1, 1)) +
+  geom_hline(yintercept = 0, alpha = 1/3, linetype = 2) +
+  labs(
+    x = "Predictors", 
+    y = "r (correlation with baseline pelvic pain)", 
+    caption = "95% CI error bars."
+  ) +
+  theme_classic() +
+  theme(legend.position = "none")
+cor_plot
+
+# models in a list
+mods <- list(year1_mod, year2_mod, year3_mod, year4_mod)
+gss_mods <- mods # for saving out and naming consistency
+#save(gss_mods, file = "../output/gss-mods.rda") # uncomment out to save
+
+# sample sizes
+mods %>%
+  map("model") %>%
+  map_dfr(~tibble(n = nrow(.x)), .id = "mod")
+
+# computes the partial eta squareds with CI
+mods_peta2 <-
+  mods %>%
+  map(
+    ~eta_squared(
+      car::Anova(.x, type = 3),
+      partial = TRUE, 
+      generalized = FALSE, 
+      ci = .95, 
+      alternative = "two.sided",
+      include_intercept = TRUE
+    )
+  ) %>%
+  map_dfr(~as_tibble(.x), .id = "dv_year") %>%
+  select(-CI) %>%
+  rename(Eta2_partial_CI_low = CI_low, Eta2_partial_CI_high = CI_high)
+
+# computes model estimates
+mods_ests <- 
+  mods %>% 
+  map_dfr(~broom::tidy(.x), .id = "dv_year") %>% 
+  rename(Parameter = term) # better for join
+
+# computes standardized regression coefficients with CI
+mods_beta <- 
+  mods %>% 
+  map_dfr(~as_tibble(standardize_parameters(.x)), .id = "dv_year") %>%
+  select(-CI) %>%
+  rename(beta = Std_Coefficient, beta_CI_low = CI_low, beta_CI_high = CI_high)
+
+# calculates the sums of squares
+mods_ss <- 
+  mods %>%
+  map_dfr(~as_tibble(car::Anova(.x, type = 3), rownames = "Parameter"), .id = "dv_year") %>%
+  rename(dfn = Df, SS = `Sum Sq`, F = `F value`, p = `Pr(>F)`)
+
+# reorganizes for the residuals (error)
+mods_ss_resids <- 
+  mods_ss %>% 
+  filter(Parameter == "Residuals") %>%
+  rename(SSE = SS, dfd = dfn) %>%
+  mutate(MSE = SSE / dfd) %>%
+  select(-F, -p, -Parameter)
+
+# includes correct SS with residuals
+mods_ss_final <- 
+  mods_ss %>% 
+  filter(Parameter != "Residuals") %>% 
+  left_join(., mods_ss_resids, by = "dv_year")
+
+# combines into one big table
+mods_res <-
+  mods_ests %>% 
+  left_join(., mods_beta, by = c("dv_year", "Parameter")) %>%
+  left_join(., mods_peta2, by = c("dv_year", "Parameter")) %>%
+  left_join(., mods_ss_final, by = c("dv_year", "Parameter")) %>%
+  mutate(Parameter = gsub("[\\(\\)]", "", Parameter)) %>%
+  mutate(sig = p.value < .05, Parameter = as.factor(Parameter)) %>%
+  mutate(
+    Parameter = fct_relevel(
+      Parameter, 
+      c("Intercept", "year0_mc", "gss_mc", "V1_mc")
+    )
+  )
+
+# PARTIAL ETA SQUARED PLOT
+this_colors <- 
+  c(
+    ghibli_palettes$MononokeMedium[3], 
+    ghibli_palettes$KikiMedium[4],
+    ghibli_palettes$PonyoMedium[3]
+    )
+pd <- position_dodge(width = .4)
+peta2_plot <- 
+  ggplot(
+    mods_res %>% filter(Parameter != "Intercept", dv_year < 5), 
+    aes(dv_year, Eta2_partial, group = Parameter, color = Parameter, shape = sig)
+  ) +
+  geom_line(position = pd, alpha = 1/2) +
+  geom_point(position = pd, size = 3) +
+  scale_color_manual(values = this_colors) +
+  scale_shape_manual(values = c(1, 16)) +
+  labs(
+    x = "Year", 
+    y = "Partial Eta^2",
+  ) +
+  theme_classic() +
+  theme(legend.position = "bottom")
+peta2_plot #plots
+
+# BETA PLOT
+beta_plot <- 
+  ggplot(
+    mods_res %>% filter(Parameter != "Intercept", dv_year < 5), #  
+    aes(dv_year, beta, group = Parameter, color = Parameter)
+  ) +
+  geom_line(position = pd, alpha = 1/2) +
+  geom_point(aes(shape = sig), position = pd, size = 3) +
+  scale_shape_manual(values = c(1, 16)) +
+  scale_color_manual(values = this_colors) +
+  labs(
+    x = "Year", 
+    y = "Beta"
+  ) +
+  coord_cartesian(ylim = c(-.5, .75)) +
+  geom_hline(yintercept = 0, alpha = 1/3, linetype = 2) +
+  theme_classic() +
+  theme(legend.position = "bottom")
+beta_plot #plots
+
+# plots side by side
+reg_res_plot <- peta2_plot | beta_plot
+reg_res_plot
+
+
--- prepro-gss.R
+++ prepro-gss.R
@@ -6,21 +6,84 @@
 # Sensitivity (GSS) measure developed by Schrepf et al. (2018) and previously
 # reported on from our lab Schrepf et al. (in press)
 
-# Prep workspace
+# Prep workspace - - - -
 source("r-prep.R")
 
-# Load data
-load("../output/arm1_screenvisit_api_parsed.RData") # arm 1 screen visit data
-load("../output/arm2_screenvisit_api_parsed.RData") # arm 2 screen visit data
+# Load data - - - -
+
+# Redcap data pulled through API
+#load("../output/arm1_screenvisit_api_parsed.RData") # arm 1 screen visit data
+#load("../output/arm2_screenvisit_api_parsed.RData") # arm 2 screen visit data
 
 # Brings in CRAMPP CODES -- a document that connects record numbers across the
 # arms/redcap projects
 crampp_codes <- read_excel("../data/crampp-codes.xlsx", sheet = "usethis")
+load("../output/mmh-res.RData") # loads in PCA data for ss numbers
 
-# FOR TODAY: USE THE EXCEL DOCUMENT OF SCREEN VISITS TO CORROBORATE FINDINGS!
+#* PROBLEM:
+#* the CMSI is an endorsement questionnaire, meaning that participants would 
+#* endorse (check a box) if they experienced a symptom. If you pull data through
+#* the Recap api, it does not report anything if a participant did not endorse a
+#* symptom. This is a problem because then you do not have any way of knowing if
+#* a participant skipped the questionnaire or if they simply did not endorse 
+#* symptom(s). When downloading the data from Redcap via Excel, it instead
+#* places a zero in the column for no endorsement regardless if they took the
+#* questionnaire or not. In sum, there is no efficient or 100% reliable method
+#* to know whether a participant completed the CMSI. This was somewhat later fixed
+#* by including a 99-no pain option on cmsi_fibro1, but this still doesn't
+#* fix the earlier cmsi questions.
+#* My temporary solution is to assume that all participants that are included in
+#* the PCA analysis (i.e., had complete QST data) completed the CMSI and use the 
+#* excel version
 
-# this was processed in `screenvisit-prepro.R` using the Excel doc of redcap
-load("../output/redcap-cmsi-data.RData") 
+# Reads in csv data (i.e., excel version)
+arm1_screen_data <- 
+  read_csv(file = "../data/arm-1-screen-data.csv") # arm 1 screen data
+arm2_screen_data <- 
+  read_csv(file = "../data/arm-2-screen-data.csv") # arm 2 screen data
+
+# preps arm 1 cmsi data
+arm1_cmsi <-
+  arm1_screen_data %>%
+  select(record_number, contains("cmsi")) %>%
+  left_join(
+    ., 
+    crampp_codes %>% select(ss, arm1r), 
+    by = c("record_number" = "arm1r")
+    ) %>%
+  relocate(ss) %>%
+  select(-record_number) %>%
+  filter(complete.cases(ss))
+
+# preps arm 2 cmsi data
+arm2_cmsi <-
+  arm2_screen_data %>%
+  select(record_number, contains("cmsi")) %>%
+  left_join(
+    ., 
+    crampp_codes %>% select(ss, arm2r), 
+    by = c("record_number" = "arm2r")
+  ) %>%
+  relocate(ss) %>%
+  select(-record_number) %>%
+  filter(complete.cases(ss))
+
+# combines arm 1 and 2 cmsi data 
+cmsi_data <- bind_rows(arm1_cmsi, arm2_cmsi) %>% arrange(ss)
+
+
+# narrows down data for 200 PCA participants - - - -
+
+# gets factor scores and ss for later
+fi <- 
+  as_tibble(pca_res$Fixed.Data$ExPosition.Data$fi, rownames = "ss") %>%
+  mutate(ss = as.numeric(ss)) %>%
+  left_join(., crampp_codes %>% select(ss, group), by = "ss") %>%
+  select(ss, group, V1:V40)
+
+
+# THE MAIN DATA FRAME FOR GSS COMPUTATIONS
+cmsi_data_pca <- cmsi_data %>% filter(ss %in% fi$ss)
 
 
 #############
@@ -29,188 +92,279 @@ load("../output/redcap-cmsi-data.RData")
 #           #
 #############
 
-# Arm 1 screen visit
-arm1_pain_site <- 
-  arm1_screenvisit_api_parsed %>% 
-  filter(field_name == "cmsi_fibro1") %>% # LAST 7 DAYS
-  left_join(., crampp_codes %>% select(ss, arm1r), by = c("record" = "arm1r")) %>%
-  select(ss, field_name, value)
+cmsi_pain_site_ss <- 
+  cmsi_data_pca %>% 
+  select(ss, starts_with("cmsi_fibro1")) %>% # pain at sites in last 7 days
+  # removes the no pain column, as can be inferred from the remaining columns
+  select(-cmsi_fibro1___99) %>%
+  pivot_longer(-ss) %>%
+  group_by(ss) %>%
+  summarise(sum = sum(value), total_possible = n()) %>%
+  ungroup() %>%
+  mutate(sub_q = "pain_sites")
 
-# Arm 2 screen visit
-arm2_pain_site <- 
-  arm2_screenvisit_api_parsed %>% 
-  filter(field_name == "cmsi_fibro1") %>% # LAST 7 DAYS
-  left_join(., crampp_codes %>% select(ss, arm2r), by = c("record" = "arm2r")) %>%
-  select(ss, field_name, value)
+###########################################
+#                                         #
+# Somatic Awareness & Sensory Sensitivity #
+#                                         #
+###########################################
 
-# combines arm 1 and 2
-pain_site_data <- 
-  bind_rows(arm1_pain_site, arm2_pain_site) %>% 
-  mutate(value = as.numeric(value))
-
-# separates participants (the reason for this is to really make sure that 
-# no participants reported no pain then endorsed a site)
-pain_site_no_pain_ss <- pain_site_data %>% filter(value == 99)
-pain_site_pain_ss <- pain_site_data %>% filter(value != 99)
-
-# Sums pain subjects
-pain_site_pain_ss_sum <- 
-  pain_site_pain_ss %>% 
-  group_by(ss) %>% 
-  summarise(pain_sites = n())
-
-# records 0 for no pain subjects
-pain_site_no_pain_ss_sum <- 
-  pain_site_no_pain_ss %>% 
-  mutate(pain_sites = 0) %>%
-  select(ss, pain_sites)
-
-# combines sums together
-pain_site_sum <- 
-  bind_rows(pain_site_pain_ss_sum, pain_site_no_pain_ss_sum) %>%
-  mutate(meas = "pain_site") %>%
-  select(ss, meas, sum = pain_sites) # helps with join later
-
-# any duplicates? answer is no
-# pain_site_sum %>% count(ss) %>% filter(n > 1)
-
-########
-#      #
-# CMSI #
-#      #
-########
-
-# The redcap API only returns items that were endorsed on the CMSI, not those 
-# that were not endorsed, making the current output ambiguous to work with
-# therefore, this workaround was devised
-
-# inits
-ss_var_index <- c(1:41)
-ss_vars <- paste0(rep("cmsi_gen", length(ss_var_index)), ss_var_index)
-
-# Arm 1 - - - -
-
-# creates tibble of all possible CMSI responses
-cmsi_placeholder <-
-  tibble(
-  record = rep(unique(arm1_screenvisit_api_parsed$record), each = length(ss_vars)),
-  cmsi = rep(ss_vars, length(unique(record)))
-)
-
-arm1_cmsi <- 
-  arm1_screenvisit_api_parsed %>%
-  filter(substr(field_name, 1, 8) == "cmsi_gen") %>%
-  mutate(value = as.numeric(value)) %>%
-  filter(value == 1) %>% # LAST 3 MONTHS
-  left_join(., crampp_codes %>% select(ss, arm1r), by = c("record" = "arm1r")) %>%
-  select(ss, cmsi = field_name)
-  
-arm2_cmsi <- 
-  arm2_screenvisit_api_parsed %>%
-  filter(substr(field_name, 1, 8) == "cmsi_gen") %>%
-  mutate(value = as.numeric(value)) %>%
-  filter(value == 1) %>% # LAST 3 MONTHS
-  left_join(., crampp_codes %>% select(ss, arm2r), by = c("record" = "arm2r")) %>%
-  select(ss, cmsi = field_name)
-
-cmsi_comb <- 
-  bind_rows(arm1_cmsi, arm2_cmsi) %>%
+sa_ss_data_ss <-
+  cmsi_data_pca %>%
+  select(ss, starts_with("cmsi_gen")) %>%
+  pivot_longer(-ss) %>%
+  separate(name, into = c("meas", "q", "time")) %>%
   mutate(
-    cmsi = as.numeric(gsub("cmsi_gen", "", cmsi)),
-    meas = case_when(
-      cmsi %in% c(35, 36, 37, 39) ~ "sens", # sensory sensitivity
-      cmsi %in% c(3,6,7,8,9,10,11,14,15,16,17,18,20,21,22,24,25) ~ "sa", # somat aware 
-      TRUE ~ as.character(cmsi)
-    )
-  )
-
-# summates the sensory sensitivity and somatic awareness data
-cmsi_data_sum <- 
-  cmsi_comb %>%
-  filter(meas %in% c("sens", "sa")) %>%
-  group_by(ss, meas) %>%
-  summarise(sum = n()) %>%
-  ungroup()
-
-load("../output/redcap-cmsi-data.RData")
-
-
-
-
-arm1_cmsi_ss <- 
-  left_join(cmsi_placeholder, arm1_cmsi, by = c("record", "cmsi")) %>%
-  mutate(value = ifelse(is.na(value), 0, 1)) %>%
-  left_join(., crampp_codes %>% select(ss, arm1r), by = c("record" = "arm1r")) %>%
-  filter(complete.cases(ss)) %>%
-  select(ss, cmsi, value)
-
-# Arm 2 - - - -
-
-# creates tibble of all possible CMSI responses
-cmsi_placeholder_2 <-
-  tibble(
-    record = rep(unique(arm2_screenvisit_api_parsed$record), each = length(ss_vars)),
-    cmsi = rep(ss_vars, length(unique(record)))
-  )
-
-
-
-arm2_cmsi_ss <- 
-  left_join(cmsi_placeholder_2, arm2_cmsi, by = c("record", "cmsi")) %>%
-  mutate(value = ifelse(is.na(value), 0, 1)) %>%
-  left_join(., crampp_codes %>% select(ss, arm2r), by = c("record" = "arm2r")) %>%
-  filter(complete.cases(ss)) %>%
-  select(ss, cmsi, value)
-
-# combines CMSI data across arms and adds labels to help with calculation
-cmsi_data <- 
-  bind_rows(arm1_cmsi_ss, arm2_cmsi_ss) %>%
-  mutate(
-    cmsi = as.numeric(gsub("cmsi_gen", "", cmsi)),
-    meas = case_when(
-      cmsi %in% c(35, 36, 37, 39) ~ "sens", # sensory sensitivity
-      cmsi %in% c(3,6,7,8,9,10,11,14,15,16,17,18,20,21,22,24,25) ~ "sa", # somat aware 
-      TRUE ~ as.character(cmsi)
+    q = as.numeric(gsub("gen", "", q)),
+     sub_q = case_when(
+      q %in% c(35, 36, 37, 39) ~ "sens", # sensory sensitivity
+      q %in% c(3,6,7,8,9,10,11,14,15,16,17,18,20,21,22,24,25) ~ "sa", # somat aware 
+      TRUE ~ as.character(q)
       )
     )
 
-# summates the sensory sensitivity and somatic awareness data
-cmsi_data_sum <- 
-  cmsi_data %>%
-  filter(meas %in% c("sens", "sa")) %>%
-  group_by(ss, meas) %>%
-  summarise(sum = sum(value)) %>%
+# calculates sum total score
+sa_ss_data_sum <- 
+  sa_ss_data_ss %>%
+  filter(sub_q %in% c("sens", "sa")) %>% # filters out questions not part of gss
+  group_by(ss, time, sub_q) %>%
+  summarise(sum = sum(value), total_possible = n()) %>%
   ungroup()
 
-# Combines all three measures of the GSS:
-gss_data <- bind_rows(cmsi_data_sum, pain_site_sum) %>% arrange(ss)
+# creating GSS
+gss_data_long <-
+  sa_ss_data_sum %>% 
+  filter(time == 1) %>% # pain in last three months
+  select(-time, -total_possible) %>%
+  bind_rows(., cmsi_pain_site_ss %>% select(-total_possible)) %>%
+  arrange(ss, sub_q)
 
-# Saves out data
-save(gss_data, file = "../output/gss-data.rda") # rda
-write_csv(gss_data, file = "../output/gss-data.csv") # csv
+# calculates GSS here and is in wide format
+gss_data_wide <-
+  gss_data_long %>% 
+  pivot_wider(id_cols = ss, names_from = "sub_q", values_from = "sum") %>%
+  mutate(
+    across(
+      .cols = c(pain_sites, sa, sens), 
+      .fns = ~as.numeric(scale(.x)), # calculation of z scores here
+      .names = "{.col}_z"),
+    gss = pain_sites_z+sa_z+sens_z # sum of z scores to create "GSS"
+    )
 
-# cleans script objects
+# Saves out GSS data - - - -
+save(gss_data_wide, file = "../output/gss-data-wide.rda")
+write_csv(gss_data_wide, file = "../output/gss-data-wide.csv")
+
+# removes script objects - - - -
 rm(
   arm1_cmsi,
-  arm1_cmsi_ss,
-  arm1_pain_site,
-  arm1_screenvisit_api_parsed,
+  arm1_screen_data,
   arm2_cmsi,
-  arm2_cmsi_ss,
-  arm2_pain_site,
-  arm2_screenvisit_api_parsed,
+  arm2_screen_data,
+  bada_res,
+  bada_res_2,
   cmsi_data,
-  cmsi_data_sum,
-  cmsi_placeholder,
-  cmsi_placeholder_2,
+  cmsi_data_pca,
+  cmsi_pain_site_ss,
   crampp_codes,
-  gss_data,
-  pain_site_data,
-  pain_site_no_pain_ss,
-  pain_site_no_pain_ss_sum,
-  pain_site_pain_ss,
-  pain_site_pain_ss_sum,
-  pain_site_sum,
-  ss_var_index,
-  ss_vars
+  fi,
+  gss_data_long,
+  gss_data_wide,
+  pca_res,
+  sa_ss_data_ss,
+  sa_ss_data_sum,
+  shrs_res,
+  iters
 )
+
+
+
+
+
+# PREVIOUS CODE SOLUTION - - - - -
+
+# # Arm 1 screen visit
+# arm1_pain_site <- 
+#   arm1_screenvisit_api_parsed %>% 
+#   filter(field_name == "cmsi_fibro1") %>% # LAST 7 DAYS
+#   left_join(., crampp_codes %>% select(ss, arm1r), by = c("record" = "arm1r")) %>%
+#   select(ss, field_name, value)
+# 
+# # Arm 2 screen visit
+# arm2_pain_site <- 
+#   arm2_screenvisit_api_parsed %>% 
+#   filter(field_name == "cmsi_fibro1") %>% # LAST 7 DAYS
+#   left_join(., crampp_codes %>% select(ss, arm2r), by = c("record" = "arm2r")) %>%
+#   select(ss, field_name, value)
+# 
+# # combines arm 1 and 2
+# pain_site_data <- 
+#   bind_rows(arm1_pain_site, arm2_pain_site) %>% 
+#   mutate(value = as.numeric(value))
+# 
+# # separates participants (the reason for this is to really make sure that 
+# # no participants reported no pain then endorsed a site)
+# pain_site_no_pain_ss <- pain_site_data %>% filter(value == 99)
+# pain_site_pain_ss <- pain_site_data %>% filter(value != 99)
+# 
+# # Sums pain subjects
+# pain_site_pain_ss_sum <- 
+#   pain_site_pain_ss %>% 
+#   group_by(ss) %>% 
+#   summarise(pain_sites = n())
+# 
+# # records 0 for no pain subjects
+# pain_site_no_pain_ss_sum <- 
+#   pain_site_no_pain_ss %>% 
+#   mutate(pain_sites = 0) %>%
+#   select(ss, pain_sites)
+# 
+# # combines sums together
+# pain_site_sum <- 
+#   bind_rows(pain_site_pain_ss_sum, pain_site_no_pain_ss_sum) %>%
+#   mutate(meas = "pain_site") %>%
+#   select(ss, meas, sum = pain_sites) # helps with join later
+# 
+# # any duplicates? answer is no
+# # pain_site_sum %>% count(ss) %>% filter(n > 1)
+# 
+# ########
+# #      #
+# # CMSI #
+# #      #
+# ########
+# 
+# # The redcap API only returns items that were endorsed on the CMSI, not those 
+# # that were not endorsed, making the current output ambiguous to work with
+# # therefore, this workaround was devised
+# 
+# # inits
+# ss_var_index <- c(1:41)
+# ss_vars <- paste0(rep("cmsi_gen", length(ss_var_index)), ss_var_index)
+# 
+# # Arm 1 - - - -
+# 
+# # creates tibble of all possible CMSI responses
+# cmsi_placeholder <-
+#   tibble(
+#   record = rep(unique(arm1_screenvisit_api_parsed$record), each = length(ss_vars)),
+#   cmsi = rep(ss_vars, length(unique(record)))
+# )
+# 
+# arm1_cmsi <- 
+#   arm1_screenvisit_api_parsed %>%
+#   filter(substr(field_name, 1, 8) == "cmsi_gen") %>%
+#   mutate(value = as.numeric(value)) %>%
+#   filter(value == 1) %>% # LAST 3 MONTHS
+#   left_join(., crampp_codes %>% select(ss, arm1r), by = c("record" = "arm1r")) %>%
+#   select(ss, cmsi = field_name)
+#   
+# arm2_cmsi <- 
+#   arm2_screenvisit_api_parsed %>%
+#   filter(substr(field_name, 1, 8) == "cmsi_gen") %>%
+#   mutate(value = as.numeric(value)) %>%
+#   filter(value == 1) %>% # LAST 3 MONTHS
+#   left_join(., crampp_codes %>% select(ss, arm2r), by = c("record" = "arm2r")) %>%
+#   select(ss, cmsi = field_name)
+# 
+# cmsi_comb <- 
+#   bind_rows(arm1_cmsi, arm2_cmsi) %>%
+#   mutate(
+#     cmsi = as.numeric(gsub("cmsi_gen", "", cmsi)),
+#     meas = case_when(
+#       cmsi %in% c(35, 36, 37, 39) ~ "sens", # sensory sensitivity
+#       cmsi %in% c(3,6,7,8,9,10,11,14,15,16,17,18,20,21,22,24,25) ~ "sa", # somat aware 
+#       TRUE ~ as.character(cmsi)
+#     )
+#   )
+# 
+# # summates the sensory sensitivity and somatic awareness data
+# cmsi_data_sum <- 
+#   cmsi_comb %>%
+#   filter(meas %in% c("sens", "sa")) %>%
+#   group_by(ss, meas) %>%
+#   summarise(sum = n()) %>%
+#   ungroup()
+# 
+# load("../output/redcap-cmsi-data.RData")
+# 
+# 
+# 
+# 
+# arm1_cmsi_ss <- 
+#   left_join(cmsi_placeholder, arm1_cmsi, by = c("record", "cmsi")) %>%
+#   mutate(value = ifelse(is.na(value), 0, 1)) %>%
+#   left_join(., crampp_codes %>% select(ss, arm1r), by = c("record" = "arm1r")) %>%
+#   filter(complete.cases(ss)) %>%
+#   select(ss, cmsi, value)
+# 
+# # Arm 2 - - - -
+# 
+# # creates tibble of all possible CMSI responses
+# cmsi_placeholder_2 <-
+#   tibble(
+#     record = rep(unique(arm2_screenvisit_api_parsed$record), each = length(ss_vars)),
+#     cmsi = rep(ss_vars, length(unique(record)))
+#   )
+# 
+# 
+# 
+# arm2_cmsi_ss <- 
+#   left_join(cmsi_placeholder_2, arm2_cmsi, by = c("record", "cmsi")) %>%
+#   mutate(value = ifelse(is.na(value), 0, 1)) %>%
+#   left_join(., crampp_codes %>% select(ss, arm2r), by = c("record" = "arm2r")) %>%
+#   filter(complete.cases(ss)) %>%
+#   select(ss, cmsi, value)
+# 
+# # combines CMSI data across arms and adds labels to help with calculation
+# cmsi_data <- 
+#   bind_rows(arm1_cmsi_ss, arm2_cmsi_ss) %>%
+#   mutate(
+#     cmsi = as.numeric(gsub("cmsi_gen", "", cmsi)),
+#     meas = case_when(
+#       cmsi %in% c(35, 36, 37, 39) ~ "sens", # sensory sensitivity
+#       cmsi %in% c(3,6,7,8,9,10,11,14,15,16,17,18,20,21,22,24,25) ~ "sa", # somat aware 
+#       TRUE ~ as.character(cmsi)
+#       )
+#     )
+# 
+# # summates the sensory sensitivity and somatic awareness data
+# cmsi_data_sum <- 
+#   cmsi_data %>%
+#   filter(meas %in% c("sens", "sa")) %>%
+#   group_by(ss, meas) %>%
+#   summarise(sum = sum(value)) %>%
+#   ungroup()
+# 
+# # Combines all three measures of the GSS:
+# gss_data <- bind_rows(cmsi_data_sum, pain_site_sum) %>% arrange(ss)
+# 
+# # Saves out data
+# save(gss_data, file = "../output/gss-data.rda") # rda
+# write_csv(gss_data, file = "../output/gss-data.csv") # csv
+# 
+# # cleans script objects
+# rm(
+#   arm1_cmsi,
+#   arm1_cmsi_ss,
+#   arm1_pain_site,
+#   arm1_screenvisit_api_parsed,
+#   arm2_cmsi,
+#   arm2_cmsi_ss,
+#   arm2_pain_site,
+#   arm2_screenvisit_api_parsed,
+#   cmsi_data,
+#   cmsi_data_sum,
+#   cmsi_placeholder,
+#   cmsi_placeholder_2,
+#   crampp_codes,
+#   gss_data,
+#   pain_site_data,
+#   pain_site_no_pain_ss,
+#   pain_site_no_pain_ss_sum,
+#   pain_site_pain_ss,
+#   pain_site_pain_ss_sum,
+#   pain_site_sum,
+#   ss_var_index,
+#   ss_vars
+# )
